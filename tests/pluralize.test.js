f00219bc8b508506c2c88c410c5b03e95c60f726
mastro-elfo <francesco.209@gmail.com>
2020-08-08 23:15:54 +0200

    test pluralize

---
+++ tests/pluralize.test.js
@@ -0,0 +1,13 @@
+import { pluralize } from "../";
+
+test("Plural with count 0", () => {
+  expect(pluralize(0, "Singular", "Plural")).toBe("Plural");
+});
+
+test("Singular with count 1", () => {
+  expect(pluralize(1, "Singular", "Plural")).toBe("Singular");
+});
+
+test("Plural with count > 1", () => {
+  expect(pluralize(2, "Singular", "Plural")).toBe("Plural");
+});
