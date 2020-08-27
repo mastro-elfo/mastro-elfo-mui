5b63de3922c44c34b108b12a061bff99af62e79c
mastro-elfo <francesco.209@gmail.com>
2020-08-27 22:26:22 +0200

    Create styles

---
+++ src/styles/danger.js
@@ -0,0 +1,23 @@
+/**
+ * Use `error` palette to create a "danger" component
+ */
+
+// https://material-ui.com/styles/api/#styled-component-styles-options-component
+import { styled } from "@material-ui/core/styles";
+
+export default Component =>
+  styled(Component)(
+    ({
+      theme: {
+        palette: {
+          error: { contrastText, main, dark }
+        }
+      }
+    }) => ({
+      color: contrastText,
+      backgroundColor: main,
+      "&:hover": {
+        backgroundColor: dark
+      }
+    })
+  );
---
+++ src/styles/index.js
@@ -0,0 +1,3 @@
+export { default as absolute } from "./absolute";
+export { default as danger } from "./danger";
+export { default as grow } from "./grow";
