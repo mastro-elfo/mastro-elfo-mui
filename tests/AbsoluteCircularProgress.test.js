fcd8906a69cf4113e238fc67b08839ab481634ce
mastro-elfo <francesco.209@gmail.com>
2020-08-27 22:14:32 +0200

    Remove unused code

--- tests/AbsoluteCircularProgress.test.js
+++ tests/AbsoluteCircularProgress.test.js
@@ -2,10 +2,6 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { AbsoluteCircularProgress } from "../";
 
-function Component() {
-  return <div />;
-}
-
 it("renders AbsoluteCircularProgress", () => {
   const tree = renderer.create(<AbsoluteCircularProgress />).toJSON();
   expect(tree).toMatchSnapshot();
