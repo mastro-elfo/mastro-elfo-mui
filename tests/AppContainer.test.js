b7f4238dc632b6f18ea0348837e3a375bf45c342
mastro-elfo <francesco.209@gmail.com>
2020-08-27 22:17:16 +0200

    Correct description

--- tests/AppContainer.test.js
+++ tests/AppContainer.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { AppContainer } from "../";
 
-it("renders AbsoluteCircularProgress", () => {
+it("renders AppContainer", () => {
   const tree = renderer.create(<AppContainer />).toJSON();
   expect(tree).toMatchSnapshot();
 });
