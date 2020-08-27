cad433d85da0ebe898bb51110bc574f3b9f470bc
mastro-elfo <francesco.209@gmail.com>
2020-08-27 22:57:41 +0200

    Remove unsure tests

--- tests/AbsoluteCircularProgress.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import AbsoluteCircularProgress from "../src/components/AbsoluteCircularProgress";
-
-it("renders AbsoluteCircularProgress", () => {
-  const tree = renderer.create(<AbsoluteCircularProgress />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
--- tests/AppContainer.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import AppContainer from "../src/components/AppContainer";
-
-it("renders AppContainer", () => {
-  const tree = renderer.create(<AppContainer />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
