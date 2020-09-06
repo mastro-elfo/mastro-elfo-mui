f2ad7e629e4eddb8bbaaf4cf77243294c98d6008
mastro-elfo <francesco.209@gmail.com>
2020-09-06 21:54:27 +0200

    Add RouterWrapper component

---
+++ src/components/RouterWrapper.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import {
+  // See https://reacttraining.com/react-router/web/guides/quick-start for details
+  MemoryRouter,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+
+export default function({
+  Router = MemoryRouter,
+  routes = [],
+  redirect = null
+}) {
+  return (
+    <Router>
+      <Switch>
+        {routes.map((route, i) => (
+          <Route key={i} {...route} />
+        ))}
+        {!!redirect && <Redirect to={redirect} />}
+      </Switch>
+    </Router>
+  );
+}
