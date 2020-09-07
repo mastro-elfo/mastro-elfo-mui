3a84a48bb8cc4d3c7ae4905281c94de78df5a55e
mastro-elfo <francesco.209@gmail.com>
2020-09-07 19:15:30 +0200

    Add ResultList component

---
+++ src/components/ResultList.jsx
@@ -0,0 +1,51 @@
+/**
+ * Organizes a List with optional SubHeader
+ *
+ * If `results` is `null` or `undefined`, then prints nothing.
+ *
+ * Otherwise `results` must be an array, for each item `mapper` is called and must provide at least a unique `key`.
+ *
+ * Also `LeftIcon` and `RightIcon` are used inside a `ListItemIcon`, and `primary` and `secondary` are used with `ListItemText`.
+ *
+ * Anything else is passed to `ListItem`. By default `onClick` sets `button` to `true`.
+ */
+
+import React from "react";
+
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader
+} from "@material-ui/core";
+
+export default function({ subheader, mapper = r => r, results, ...others }) {
+  if (results === null || results === undefined) return null;
+  return (
+    <List
+      subheader={!!subheader && <ListSubheader>{subheader}</ListSubheader>}
+      {...others}
+    >
+      {results.map(r => {
+        const {
+          LeftIcon,
+          RightIcon,
+          primary,
+          secondary,
+          onClick,
+          ...others
+        } = mapper(r);
+        return (
+          <ListItem button={!!onClick} onClick={onClick} {...others}>
+            {!!LeftIcon && <ListItemIcon>{LeftIcon}</ListItemIcon>}
+
+            <ListItemText primary={primary} secondary={secondary} />
+
+            {!!RightIcon && <ListItemIcon>{RightIcon}</ListItemIcon>}
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -17,9 +17,12 @@ export { default as NotifyWrapper } from "./NotifyWrapper";
 export { default as Page } from "./Page";
 export { default as PasswordField } from "./PasswordField";
 export { NoPrint, Print } from "./Print";
-export { default as Router } from "./Router";
+export { default as ResultList } from "./ResultList";
 export { default as RouterWrapper } from "./RouterWrapper";
 export { default as SearchField } from "./SearchField";
 export { default as TableHeadCell } from "./TableHeadCell";
 export { default as ThemeWrapper } from "./ThemeWrapper";
 export { default as TopFab } from "./TopFab";
+
+// Deprecated
+export { default as Router } from "./Router";
