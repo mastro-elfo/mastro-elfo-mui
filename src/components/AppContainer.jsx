516d23b558bf7c9a8de4356a802f68120f41c817
mastro-elfo <francesco.209@gmail.com>
2020-09-10 18:52:38 +0200

    Remove router (see comment); Add PropTypes

--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -1,27 +1,28 @@
 /**
  * Main container for an application.
  *
- * Wraps the application with Theme, Error handler, Notifier and Router.
- * Wrappers can be configured with ThemeProps, NotifyProps and RouterProps.
+ * Wraps the application with Theme, Error handler, Notifier.
+ * Wrappers can be configured with ThemeProps, NotifyProps.
+ * Router, if used, must be passed as children, because other component can be added that need to wrap the router.
  */
 
 import React from "react";
+import PropTypes from "prop-types";
 
-import { ErrorWrapper, NotifyWrapper, RouterWrapper, ThemeWrapper } from "./";
+import { ErrorWrapper, NotifyWrapper, ThemeWrapper } from "./";
 
-export default function App({
-  children,
-  ThemeProps = {},
-  NotifyProps = {},
-  RouterProps = {}
-}) {
+export default function App({ children, ThemeProps = {}, NotifyProps = {} }) {
   return (
     <ThemeWrapper {...ThemeProps}>
       <ErrorWrapper>
-        <NotifyWrapper {...NotifyProps}>
-          <RouterWrapper {...RouterProps}>{children}</RouterWrapper>
-        </NotifyWrapper>
+        <NotifyWrapper {...NotifyProps}>{children}</NotifyWrapper>
       </ErrorWrapper>
     </ThemeWrapper>
   );
 }
+
+App.propTypes = {
+  ErrorWrapper: PropTypes.object,
+  NotifyWrapper: PropTypes.object,
+  ThemeWrapper: PropTypes.object
+};
