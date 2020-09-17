c52eed0b970408044b49af1512fc2641fdca6602
mastro-elfo <francesco.209@gmail.com>
2020-09-17 11:43:29 +0200

    Add pop method and undefined length list

--- src/utils/useLilo.js
+++ src/utils/useLilo.js
@@ -2,26 +2,36 @@
  * Creates a LastInLastOut list of length `length`
  * @param  {Array}  [start=[]] List initialization
  * @param  {Number} [length=1] Length of the list
- * @return {Array}             [List, push method]
+ * @return {Array}             [list, push, pop]
  */
 
 export default function useLilo(start = [], length = 1) {
+  if (length === 0) {
+    console.warn("A list with length 0 is always empty");
+  }
   // Setup
   const [lilo, setLilo] = useState(slice(start, length));
   // Push method
   const push = item => {
-    // Push new item
-    lilo.push(item);
-    // Slice and set
-    setLilo(slice(lilo, length));
+    // Append, slice and set
+    setLilo(slice([...lilo, item], length));
   };
-  // TODO: Add pop method that pops from the beginning
-  // Return lilo list and push method
-  return [lilo, push];
+  // Pop
+  const pop = () => {
+    // Extract
+    const [item, ...rest] = lilo;
+    // Set
+    setLilo(rest);
+    // Return
+    return item;
+  };
+  // Return lilo list, push method, and pop
+  return [lilo, push, pop];
 }
 
 // Slice list to length
 function slice(list, length) {
-  // TODO: if length is undefined, don't slice [requires pop method]
-  return list.slice(Math.max(0, list.length - length));
+  // If length is undefined don't slice
+  if (length !== undefined) return list.slice(-length);
+  return list;
 }
