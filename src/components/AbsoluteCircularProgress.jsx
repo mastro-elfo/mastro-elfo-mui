835481ab9ebb74927eab6bfda55617d7cb52be24
mastro-elfo <francesco.209@gmail.com>
2020-10-12 11:44:18 +0200

    Update doc string

--- src/components/AbsoluteCircularProgress.jsx
+++ src/components/AbsoluteCircularProgress.jsx
@@ -3,5 +3,7 @@ import absolute from "../styles/absolute";
 
 /**
  * A `CircularProgress` styled with `absolute`.
+ *
+ * This is useful inside buttons.
  */
 export default absolute(CircularProgress);
