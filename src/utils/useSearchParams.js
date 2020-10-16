82ab6a8b62b0e8fe8162247659a9f1a75158214c
mastro-elfo <francesco.209@gmail.com>
2020-10-16 11:21:07 +0200

    Removed mixed exports

--- src/utils/useSearchParams.js
+++ src/utils/useSearchParams.js
@@ -1,17 +1,8 @@
 import { useLocation } from "react-router-dom";
 
+import reducer from "./reduceSearchParams";
+
 export default function useSearchParams() {
   const { search } = useLocation();
   return reducer(search);
 }
-
-export const reducer = search =>
-  search
-    // Remove the first "?" character
-    .substr(1)
-    // Split each pair
-    .split("&")
-    // Split key/value
-    .map(pair => pair.split("="))
-    // Reduce to object
-    .reduce((carry, [key, value]) => ({ ...carry, [key]: value }), {});
