7eb3e95abdf6ec3e2ce05575152a2aed1f6342e1
mastro-elfo <francesco.209@gmail.com>
2020-10-18 12:34:17 +0200

    - [NEW] component `FixedBottomNavigation`

---
+++ src/components/FixedBottomNavigation.jsx
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+
+import { useTheme } from "@material-ui/core/styles";
+import { BottomNavigation } from "@material-ui/core";
+
+function FixedBottomNavigation({ children, ...props }) {
+  const theme = useTheme();
+  return (
+    <Fragment>
+      <BottomNavigation
+        style={{
+          position: "fixed",
+          bottom: 0,
+          width: "100%",
+          zIndex: theme.zIndex.appBar
+        }}
+        {...props}
+      >
+        {children}
+      </BottomNavigation>
+      <BottomNavigation />
+    </Fragment>
+  );
+}
