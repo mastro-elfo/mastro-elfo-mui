08d599ae5165c04a6f826fc67f431a1b1d4e984d
mastro-elfo <francesco.209@gmail.com>
2020-10-21 12:05:23 +0200

    Write doc

--- src/utils/usePalette.js
+++ src/utils/usePalette.js
@@ -1,7 +1,21 @@
 import { createContext, useContext } from "react";
 
+/**
+ * Used only in `ThemeWrapper`
+ *
+ * Use `usePalette`
+ *
+ * @type {object}
+ */
 export const Context = createContext({});
 
+/**
+ * Hook that returns the current *palette*
+ *
+ * Returns an array like react's `useState`
+ *
+ * @return {array} `[palette, setPalette]`
+ */
 export function usePalette() {
   return useContext(Context);
 }
