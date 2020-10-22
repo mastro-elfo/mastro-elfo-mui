9037d86fa01c849c9de128ecd0a3af0a21e67a1c
mastro-elfo <francesco.209@gmail.com>
2020-10-22 10:41:23 +0200

    [FIX] Export `usePalette` in **utils/index.js**

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,4 +5,5 @@ export { default as shorten } from "./shorten";
 export { default as unique } from "./unique";
 export { default as useCountdown } from "./useCountdown";
 export { default as useLilo } from "./useLilo";
+export { usePalette } from "./usePalette";
 export { default as useSearchParams } from "./useSearchParams";
