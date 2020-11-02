50eee4da180cdde581e101b9398e9227a618c9e0
mastro-elfo <francesco.209@gmail.com>
2020-11-02 16:06:36 +0100

    :sparkles: `BackIconButton` has new property `IconProps`

--- src/components/BackIconButton.jsx
+++ src/components/BackIconButton.jsx
@@ -8,15 +8,17 @@ import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 /**
  * `IconButton` with `ArrowBack` icon.
  * Calls history's `goBack` when clicked.
+ * @param   {Number} back=1 How many pages should go back
+ * @param   {Object} IconProps={} Properties for `ArrowBackIcon`
  * @param  {Object} props Props are spread to `IconButton`
  * @return {node}
  */
 
-export default function({ back = 1, ...props }) {
+export default function({ back = 1, IconProps = {}, ...props }) {
   const { go } = useHistory();
   return (
     <IconButton onClick={() => go(-parseInt(back))} {...props}>
-      <ArrowBackIcon />
+      <ArrowBackIcon {...IconProps} />
     </IconButton>
   );
 }
