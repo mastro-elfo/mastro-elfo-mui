80bab38e3d2412f68bead08d8ce69e70108ce6e0
mastro-elfo <francesco.209@gmail.com>
2020-11-16 15:35:21 +0100

    :boom: :put_litter_in_its_place: `IconButtonProps` in `DrawerIconButton`

--- src/components/DrawerIconButton.jsx
+++ src/components/DrawerIconButton.jsx
@@ -10,30 +10,19 @@ import Drawer from "./Drawer";
 /**
  * [DrawerIconButton description]
  * @param       {node} children             [description]
- * @param       {Object} [IconButtonProps={}] [description]
  * @param       {Object} [DrawerProps={}}]    [description]
  * @constructor
  */
 export default function DrawerIconButton({
   children,
-  IconButtonProps = {},
   DrawerProps = {},
   ...rest
 }) {
-  if (
-    typeof IconButtonProps === "object" &&
-    Object.keys(IconButtonProps).length > 0
-  ) {
-    console.warn(
-      "IconButtonProps is deprecated since v1.23.0 and will be removed in v2.0.0. Use `<DrawerIconButton ...rest/>` instead."
-    );
-  }
-
   const [open, setOpen] = useState(false);
 
   return (
     <Fragment>
-      <IconButton onClick={() => setOpen(true)} {...IconButtonProps} {...rest}>
+      <IconButton onClick={() => setOpen(true)} {...rest}>
         <MenuIcon />
       </IconButton>
       <Drawer
@@ -50,6 +39,5 @@ export default function DrawerIconButton({
 
 DrawerIconButton.propTypes = {
   children: PropTypes.node,
-  IconButtonProps: PropTypes.object,
-  DrawerProps: PropTypes.object
+  DrawerProps: PropTypes.object,
 };
