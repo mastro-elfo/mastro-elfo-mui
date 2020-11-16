38088c45f7e371e58aae874018d30ef2ea037be6
mastro-elfo <francesco.209@gmail.com>
2020-11-16 15:39:02 +0100

    :boom: :put_litter_in_its_place: `Component` in `Push`

--- src/components/Push.jsx
+++ src/components/Push.jsx
@@ -3,37 +3,17 @@ import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
 
 /**
- * Renders `Component` with `onClick` prop that pushes or replaces history with `href`.
+ * Renders child component with `onClick` prop that pushes or replaces history with `href`.
  *
  * @param {any}     children      [description]
  * @param {string}  href          location to push or replace
  * @param {Boolean} replace=false If `true` uses `replace` instead of `push`
- * @param {node}    Component     Component rendered, deprecated
  * @param {any}     rest          Passed to Component
  * @constructor
  */
 
-export default function Push({
-  children,
-  href,
-  replace = false,
-  Component,
-  ...rest
-}) {
+export default function Push({ children, href, replace = false, ...rest }) {
   const { push, replace: repl } = useHistory();
-
-  // If `Component` is used, render the old version
-  if (Component !== undefined) {
-    console.warn(
-      "`Component` prop is deprecated since version 1.25.0 and will be removed in version 2.0.0, use `children` instead."
-    );
-
-    return (
-      <Component onClick={() => (replace ? repl(href) : push(href))} {...rest}>
-        {children}
-      </Component>
-    );
-  }
 
   // Define callback
   const handleClick = () => (replace ? repl(href) : push(href));
@@ -42,7 +22,7 @@ export default function Push({
   if (isValidElement(children)) {
     return cloneElement(children, {
       onClick: handleClick,
-      ...rest
+      ...rest,
     });
   }
 
@@ -50,7 +30,7 @@ export default function Push({
   return createElement("span", {
     children,
     onClick: handleClick,
-    ...rest
+    ...rest,
   });
 }
 
@@ -58,5 +38,4 @@ Push.propTypes = {
   children: PropTypes.any,
   href: PropTypes.string,
   replace: PropTypes.bool,
-  Component: PropTypes.elementType
 };
