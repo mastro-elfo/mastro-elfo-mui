01265152f643fe300f80cf7ee4579962225dd3c8
mastro-elfo <francesco.209@gmail.com>
2021-01-25 07:16:16 +0100

    new functions load/store/remove

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -11,6 +11,11 @@
  * @return {any}                Parsed JSON value
  */
 if (!("getJson" in Storage.prototype)) {
+  // TODO: deprecate
+  console.warn(
+    "This method is deprecated since v2.10.0 and will be removed in v3.0.0. Use `load` instead."
+  );
+
   Storage.prototype.getJson = function (key, defaultValue) {
     try {
       return JSON.parse(this.getItem(key)) || defaultValue;
@@ -29,7 +34,64 @@ if (!("getJson" in Storage.prototype)) {
  * @param  {any} value Anything that can be converted to JSON string
  */
 if (!("setJson" in Storage.prototype)) {
+  // TODO: deprecate
+  console.warn(
+    "This method is deprecated since v2.10.0 and will be removed in v3.0.0. Use `store` instead."
+  );
+
   Storage.prototype.setJson = function (key, value) {
     this.setItem(key, JSON.stringify(value));
   };
+}
+
+/**
+ * [load description]
+ * @param  {[type]} key          [description]
+ * @param  {[type]} defaultValue [description]
+ * @param  {[type]} temporary    [description]
+ * @return {[type]}              [description]
+ */
+export function load(key, defaultValue, temporary = false) {
+  // Define storage
+  const storage = temporary ? sessionStorage : localStorage;
+  // If key is not set
+  if (!storage.hasOwnProperty(key)) {
+    return defaultValue;
+  }
+  // Otherwise
+  try {
+    return JSON.parse(storage.getItem(key));
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      return defaultValue;
+    }
+    throw e;
+  }
+}
+
+/**
+ * [store description]
+ * @param  {[type]}  key               [description]
+ * @param  {[type]}  value             [description]
+ * @param  {Boolean} [temporary=false] [description]
+ * @return {[type]}                    [description]
+ */
+export function store(key, value, temporary = false) {
+  // Define storage
+  const storage = temporary ? sessionStorage : localStorage;
+  // Set item
+  storage.setItem(key, JSON.stringify(value));
+}
+
+/**
+ * [remove description]
+ * @param  {[type]}  key               [description]
+ * @param  {Boolean} [temporary=false] [description]
+ * @return {[type]}                    [description]
+ */
+export function remove(key, temporary = false) {
+  // Define storage
+  const storage = temporary ? sessionStorage : localStorage;
+  // Remove item
+  storage.removeItem(key);
 }
